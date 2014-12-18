d498b7ddf923f0b70f895f583838ce724e158b0a
Philip Withnall <philip.withnall@collabora.co.uk>
2014-12-18 10:18:03 +0000

    safety-save: Use XDG user data directory for saving the files
    Let’s not proliferate more dot-dirs.

--- safety_save.py
+++ safety_save.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from os import environ, makedirs, unlink, listdir, rmdir
 from os.path import expanduser, join, exists
 
-from gi.repository import GObject, Gedit
+from gi.repository import GLib, GObject, Gedit
 from gi.repository.Gio import Settings
 
 _log = logging.getLogger('SafetySave')
@@ -19,11 +19,10 @@ logging.debug("")
 _SETTINGS_KEY = "org.gnome.gedit.preferences.editor"
 _MAX_STORED_AGE_S = 86400 * 7 * 4
 _DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
-_PREF_DIR_NAME = '.gedit-unsaved'
 
 _gedit_settings = Settings(_SETTINGS_KEY)
 _start_timestamp = datetime.utcnow().strftime(_DATETIME_FORMAT)
-_store_root = join(expanduser('~'), _PREF_DIR_NAME)
+_store_root = join(GLib.get_user_data_dir(), 'gedit', 'unsaved')
 _store_path = join(_store_root, _start_timestamp)
 
 
